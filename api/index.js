6cc985ed4c41084bfa36d4daa48c65220c1453ba
SMCominotti <smcominotti@gmail.com>
2023-08-22 02:05:34 -0300

    prueba deploy

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+// const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({alter: true }).then(() => {
