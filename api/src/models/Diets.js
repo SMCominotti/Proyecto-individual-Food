3c4f0915b5f34572253ea86f272e4592434d6eeb
SMCominotti <smcominotti@gmail.com>
2023-08-22 02:10:00 -0300

    vuelta

---
+++ api/src/models/Diets.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  
+  sequelize.define('diets', {
+    name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+    },
+    id: {
+        type: DataTypes.UUID,  //de esta forma no se va a pisar con los ID de la API
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,//se van a crear automaticamente cuando creo una dieta
+        allowNull: false,
+    },
+    created: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+    },
+},{timestamps:false});
+};
